652e98d9b544ea821ecd4ccdd354112c1c28a4fb
paulinababalunga <paulina.babalunga@gmail.com>
2014-11-05 10:39:23 +0200

    add category

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -90,13 +90,13 @@ $(document).ready(function () {
 });
 
 var initEventHandlers = function () {
-
+  //Add a new style class when used mobile
   $(".btn-navbar").click(function () {
     $("#todo_list").toggleClass("newStyle");
   });
 
 
-  //add new reminder
+  //Add new reminder
   $("#add").click(function () {
     $(".modal-header h3").text("Add new reminder");
     app.config.editReminder = {
@@ -104,6 +104,65 @@ var initEventHandlers = function () {
       categoryId: app.config.selectedCategory
     };
   });
+
+  //Delete reminder from list
+  $('#todo_list').on('click', '.remove', function () {
+    var reminderNo = $(this).parent().data('reminderId');
+    var categoryNo = $(this).parent().data('categoryId');
+    delete app.data[categoryNo].reminders[reminderNo];
+    $(this).parent().remove();
+    localStorage['app'] = JSON.stringify(app);
+  });
+
+  //Click on category button and change title
+  $(".btnCategory").click(function () {
+    $(".modal-header h3").text("Add new category");
+
+  });
+
+
+  //Save new reminder in list
+  $('#btn-submit-reminder').click(function () {
+    save_todo();
+    location.reload();
+
+  });
+
+
+  //Save new category in list
+  $('#btn-submit-category').click(function () {
+    saveNewCategory();
+    location.reload();
+  });
+
+}
+
+//Init function
+var init = function () {
+
+  addReminders(app.config.selectedCategory);
+  addCategories();
+  clock();
+  initEventHandlers();
+
+};
+
+//Add new reminder
+function addReminders(category) {
+  if (category != undefined) {
+    $("#todo_list").html('');
+    for (var reminder in app.data[category].reminders) {
+      $("#todo_list").append(
+        '<li class="row li-row" id="reminder-' + reminder + '" data-reminder-id="' + reminder + '" data-category-id="' + category + '"' + /*(app.data[category].reminders[reminder].isDone ? 'style="display:none"' : '')*/ '' + '>' +
+        '<input type="checkbox" class="checkboxInput" ' + (app.data[category].reminders[reminder].isDone ? 'checked' : '') + '>' +
+        '<a href="#"  >' + app.data[category].reminders[reminder].description + ' - ' + new Date(app.data[category].reminders[reminder].date).toLocaleString() + '</a>' +
+          //'<a href="#"  class="remove">Remove</a>' +
+        '<span class="glyphicon glyphicon-remove remove"></span>' +
+        '<span class="glyphicon glyphicon-pencil modify" data-toggle="modal" data-target="#addReminderModal"></span>' +
+        '</li>');
+      localStorage['app'] = JSON.stringify(app);
+    }
+  }
 
 
   //Edit a reminder
@@ -121,7 +180,7 @@ var initEventHandlers = function () {
   });
 
 
-//check a reminder and hide it
+  //check a reminder and hide it
   $("#todo_list li input[type=checkbox]").click(function () {
     //if ($(this).is(':checked')) {
     var reminderNo = $(this).parent().data('reminderId');
@@ -140,76 +199,22 @@ var initEventHandlers = function () {
     //    localStorage['app'] = JSON.stringify(app);
     //}
   });
-
-
-  //delete reminder from list
-  $('#todo_list').on('click', '.remove', function () {
-    var reminderNo = $(this).parent().data('reminderId');
-    var categoryNo = $(this).parent().data('categoryId');
-    delete app.data[categoryNo].reminders[reminderNo];
-    $(this).parent().remove();
-    localStorage['app'] = JSON.stringify(app);
-  });
-
-  //Add new category
-  $(".btnCategory").click(function () {
-    $(".modal-header h3").text("Add new category");
-  });
-
-
-  //Save new reminder in list
-  $('#btn-submit-reminder').click(function () {
-    save_todo();
-    location.reload();
-  });
-
-  //Save new category in list
-  $('#btn-submit-category').click(function () {
-    saveNewCategory();
-    location.reload();
-  });
-
 }
 
-var init = function () {
-
-  addReminders(app.config.selectedCategory);
-  addCategories();
-  initEventHandlers();
-
-};
-
-
-function addReminders(category) {
-  if (category != undefined) {
-    $("#todo_list").html('');
-    for (var reminder in app.data[category].reminders) {
-      $("#todo_list").append(
-        '<li class="row li-row" id="reminder-' + reminder + '" data-reminder-id="' + reminder + '" data-category-id="' + category + '"' + /*(app.data[category].reminders[reminder].isDone ? 'style="display:none"' : '')*/ '' + '>' +
-        '<input type="checkbox" class="checkboxInput" ' + (app.data[category].reminders[reminder].isDone ? 'checked' : '') + '>' +
-        '<a href="#"  >' + app.data[category].reminders[reminder].description + ' - ' + new Date(app.data[category].reminders[reminder].date).toLocaleString() + '</a>' +
-          //'<a href="#"  class="remove">Remove</a>' +
-        '<span class="glyphicon glyphicon-remove remove"></span>' +
-        '<span class="glyphicon glyphicon-pencil modify" data-toggle="modal" data-target="#addReminderModal"></span>' +
-        '</li>');
-      localStorage['app'] = JSON.stringify(app);
-    }
-  }
-}
-
+// Add new category
 function addCategories() {
   //clear UI
   $('.nav-tag').html();
   //add category
   for (var category in app.data) {
     $('.nav-tag').append(
-      '<li data-category="' + category + '" data-title="' + app.data[category].category + '"'  + '>'  + '<span class="liStyle">' + app.data[category].category + '</span>' + '<span class="glyphicon glyphicon-remove removeCategory"></span>' +
+      '<li data-category="' + category + '" data-title="' + app.data[category].category + '"' + '>' + '<span class="liStyle">' + app.data[category].category + '</span>' + '<span class="glyphicon glyphicon-remove removeCategory"></span>' +
       '<span class="glyphicon glyphicon-pencil modifyCategory" data-toggle="modal" data-target="#addNewCategory"></span>' +
       '</li>');
     localStorage['app'] = JSON.stringify(app);
   }
 
-
+  // Click on list of categories and view reminders
   $(".my-navbar .nav-div ul li").click(function () {
     var categoryId = $(this).data('category');
     var categoryTitle = $(this).data('title');
@@ -219,49 +224,58 @@ function addCategories() {
   });
 
 
-  //delete category from list
-   $('.removeCategory').click(function () {
+  //Delete category from list
+  $('.removeCategory').click(function () {
     //var reminderNo = $(this).parent().data('reminderId');
     var categoryNo = $(this).parent().data('category');
     delete app.data[categoryNo];
     $(this).parent().remove();
+    //app.config.selectedCategory = categoryNo;
     localStorage['app'] = JSON.stringify(app);
   });
 
-
+  //Modify category
   $(".modifyCategory").click(function () {
     $(".modal-header h3").text("Edit Category");
-    //app.config.editReminder = {
-    //  //reminderId: $(this).parent().data('reminderId'),
-    //  categoryId: $(this).parent().data('categoryId')
-    //};
-    //$("#textInputCategory").val(app.data[categoryId].reminders[app.config.editReminder.reminderId].category);
-    //$("#dateInput").val(app.data[app.config.editReminder.categoryId].reminders[app.config.editReminder.reminderId].date);
-    //$("form input[type=radio]:checked").val(app.data[app.config.editReminder.categoryId].reminders[app.config.editReminder.reminderId].priorityLevel);
+    app.config.editReminder = {
+      //  //reminderId: $(this).parent().data('reminderId'),
+      categoryId: $(this).parent().data('category')
+    };
+    $("#textInputCategory").val(app.data[app.config.editReminder.categoryId].category);
   });
-
-
-    //for (var category in app.data) {
-  //  $('<li>').text(app.data[category].category)
-  //    .attr('data-category', category)
-  //    .attr('data-title', app.data[category].category)
-  //    .appendTo('.nav-tag');
-  //}
-
-
 }
 
+//Function which saves new category
 function saveNewCategory() {
   var category = {
     category: $("#textInputCategory").val(),
     reminders: {}
   };
-  app.data[Object.keys(app.data).length] = category;
+  var categoryId = app.config.editReminder.categoryId;
+  //var reminderId = app.config.editReminder.reminderId;
+  alert(categoryId)
+
+  if (categoryId) { // Modify a category
+    app.data[categoryId] = category;
+  } else { // Add it
+    app.data[Object.keys(app.data).length] = category;
+  }
+  //app.data[Object.keys(app.data).length] = category;
   localStorage['app'] = JSON.stringify(app);
   $("#addNewCategory").modal('hide');
+
+
+  //  var category = {
+//    category: $("#textInputCategory").val(),
+//    reminders: {}
+//  };
+//  app.data[Object.keys(app.data).length] = category;
+//  localStorage['app'] = JSON.stringify(app);
+//  $("#addNewCategory").modal('hide');
+//}
 }
 
-// Function which saves the data from the modal
+// Function which saves a new reminder
 function save_todo() {
   var reminder = {
     description: $("#textInput").val(),
@@ -295,6 +309,8 @@ function dateSanitizer(date) {
   return new Date(date.getTime() + userOffset).getTime();
 
 }
+
+//Notification and calculate time
 function clock() {
   var date = new Date();
   var timeDate = date.getTime();
